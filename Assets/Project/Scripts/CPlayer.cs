212079ff2219cc32c3c419f6b8a1268e79ff8750
Brunhild <nechwi1121@gmail.com>
2018-10-18 14:11:47 +0900

    Input Basics

---
+++ Assets/Project/Scripts/CPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPlayer : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    private Rigidbody2D _rigidbody = null;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        if (hAxis < 0)
+        {
+            _rigidbody.velocity = new Vector2(-speed, 0);
+        }
+        else if (hAxis > 0)
+        {
+            _rigidbody.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            _rigidbody.velocity = Vector2.zero;
+        }
+    }
+}
