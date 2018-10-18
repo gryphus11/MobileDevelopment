81cc3891d07f3aff00081ab2678ebd7ec274180b
Brunhild <nechwi1121@gmail.com>
2018-10-18 15:54:46 +0900

    Screen Bound Calculation

--- Assets/Project/Scripts/CPlayer.cs
+++ Assets/Project/Scripts/CPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CPlayer : MonoBehaviour
 {
     public float speed = 10.0f;
+    public float sizeOffset = 0.0f;
 
     private Rigidbody2D _rigidbody = null;
 
@@ -22,6 +23,16 @@ public class CPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    private void FixedUpdate()
+    {
+        MoveHorizontal();
+    }
+
+    private void MoveHorizontal()
+    {
         float hAxis = Input.GetAxis("Horizontal");
         if (hAxis < 0)
         {
@@ -33,7 +44,32 @@ public class CPlayer : MonoBehaviour
         }
         else
         {
-            _rigidbody.velocity = Vector2.zero;
+            if (Input.GetMouseButton(0))
+            {
+                Vector2 relativeMousePosition = new Vector2(Input.mousePosition.x / Screen.width, Input.mousePosition.y / Screen.height);
+                if (relativeMousePosition.x < 0.5f)
+                {
+                    _rigidbody.velocity = new Vector2(-speed, 0);
+                }
+                else if (relativeMousePosition.x > 0.5f)
+                {
+                    _rigidbody.velocity = new Vector2(speed, 0);
+                }
+            }
+            else
+            {
+                _rigidbody.velocity = Vector2.zero;
+            }
+        }
+
+        float screenLimit = (Camera.main.orthographicSize * Camera.main.aspect) - sizeOffset;
+        if (transform.position.x > screenLimit)
+        {
+            transform.position = new Vector3(screenLimit, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.x < -screenLimit)
+        {
+            transform.position = new Vector3(-screenLimit, transform.position.y, transform.position.z);
         }
     }
 }
