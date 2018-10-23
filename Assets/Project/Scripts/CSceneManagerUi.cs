ef2209339c35aedf6bce001053af3b248e1fe711
Brunhild <nechwi1121@gmail.com>
2018-10-23 11:41:57 +0900

    Build Settings

---
+++ Assets/Project/Scripts/CSceneManagerUi.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CSceneManagerUi : MonoBehaviour
+{
+    public void OnPlay()
+    {
+        SceneManager.LoadScene("3DSpace");
+    }
+}
